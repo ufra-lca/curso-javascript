30b0c0fa2d43d8a05cf208b9ca4db74178ebe4ba
rhpessoa <rhpessoa29@gmail.com>
2019-04-17 18:40:09 -0300

    Setup ES6

---
+++ DOM/build/main.bundle.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var numero = function numero(n) {
+    _classCallCheck(this, numero);
+
+    this.n = n;
+};
+
+;
+var num = new numero(19);
+console.log(num.n);
---
+++ DOM/index.js
@@ -0,0 +1,8 @@
+
+class numero  {
+    constructor(n){
+        this.n = n;
+    }
+};
+let num = new numero(19);
+console.log(num.n);
