aab790a329fa442180f7105b1dd1db2bd7b4fd84
rhpessoa <rhpessoa29@gmail.com>
2019-04-24 18:48:01 -0300

    add comentarios

--- DOM/src/carro.js
+++ DOM/src/carro.js
@@ -10,5 +10,16 @@ export default class Carro{
     setModelo(modelo){
         this._modelo = modelo;
     }
-
+    getFabricante(){
+        return this._fabricante;
+    }
+    setFabricante(fabricante){
+        this._fabricante = fabricante;
+    }
+    getAno(){
+        return this._ano;
+    }
+    setAno(ano){
+        this._ano = ano;
+    }
 }
--- DOM/src/index.js
+++ DOM/src/index.js
@@ -1,34 +1,70 @@
 //CONTROLLER
 import Carro from './carro';
 class App {
-    constructor(){
+    constructor() {
         this.listaCarros = [];
     };
-    startApp(){
-        this.adicionaCarro();
+
+    //Inicio.
+    startApp() {
+        this.exibirCarro();
         this.render();
     }
 
-    adicionaCarro(){
-        let c1 = new Carro('uno', 'fiat','2000');
-        let c2 = new Carro('gol','ww', '1992');
+    //Pega as informações add e manda para a lista de objetos.
+    exibirCarro() {
+        let c1 = new Carro('uno', 'fiat', '2000');
+        let c2 = new Carro('gol', 'ww', '1992');
         this.listaCarros.push(c1);
         this.listaCarros.push(c2);
     }
 
-    render(){
+    //Adiciona informações a serem cadastradas.
+    cadastroCarro(){
+
+    }
+
+    //Renderiza as informações.
+    render() {
 
         let ulElem = document.getElementById('lista-carro');
         ulElem.setAttribute('value', '');
-        
-        this.listaCarros.forEach( carro => { 
+
+        this.listaCarros.forEach(carro => {
+
             let liElement = document.createElement('li');
+
             let modelo = carro.getModelo();
+
+            let fabricante = carro.getFabricante();
             
-            let textElent = document.createTextNode('modelo: ' + modelo);
-            liElement.appendChild(textElent);
+            let ano = carro.getAno();
+
+
+            //criando paragrafo com o modelo.
+            let pElementM = document.createElement('p');
+            let textElementModelo = document.createTextNode('Modelo: ' + modelo);
+            pElementM.appendChild(textElementModelo);
+
+            //criando paragrafo com o fabricante.
+            let textElementFabricante = document.createTextNode('Fabricante: ' + fabricante);
+            let pElementF = document.createElement('p');
+            pElementF.appendChild(textElementFabricante);
+
+            //criando paragrafo com o ano.
+            let textElementAno = document.createTextNode('Ano:'+ ano);
+            let pElementA = document.createElement('p');
+            pElementA.appendChild(textElementAno);
+
+            //Juntando os paragrafos e fazendo uma lista.
+            liElement.appendChild(pElementF);
+            liElement.appendChild(pElementM);
+            liElement.appendChild(pElementA);
             ulElem.appendChild(liElement);
-            
+
+
+
+
         });
 
 
