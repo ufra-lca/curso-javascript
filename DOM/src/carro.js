71057e2231d1980b70045aebe33a5d156a94eb70
rhpessoa <rhpessoa29@gmail.com>
2019-04-29 21:39:37 -0300

    Algumas mudanças

--- DOM/src/carro.js
+++ DOM/src/carro.js
@@ -1,29 +1,29 @@
-export default class Carro{
-    constructor(id,modelo,fabricante, ano){
-        this.id = id;
-        this._modelo = modelo;
-        this._fabricante = fabricante;
-        this._ano = ano;
-    }
-    getId(){
-        return this.id;
-    }
-    getModelo(){
-        return this._modelo;
-    }
-    setModelo(modelo){
-        this._modelo = modelo;
-    }
-    getFabricante(){
-        return this._fabricante;
-    }
-    setFabricante(fabricante){
-        this._fabricante = fabricante;
-    }
-    getAno(){
-        return this._ano;
-    }
-    setAno(ano){
-        this._ano = ano;
-    }
+export default class Carro {
+  constructor(id, modelo, fabricante, ano) {
+    this.id = id;
+    this._modelo = modelo;
+    this._fabricante = fabricante;
+    this._ano = ano;
+  }
+  getId() {
+    return this.id;
+  }
+  getModelo() {
+    return this._modelo;
+  }
+  setModelo(modelo) {
+    this._modelo = modelo;
+  }
+  getFabricante() {
+    return this._fabricante;
+  }
+  setFabricante(fabricante) {
+    this._fabricante = fabricante;
+  }
+  getAno() {
+    return this._ano;
+  }
+  setAno(ano) {
+    this._ano = ano;
+  }
 }
